8a9316cae1619633409be4b6fcb8a24f90f3f805
sumand <suman.das@fractalanalytics.com>
2017-01-06 17:42:44 +0530

    Initial Commit

--- src/main/java/ai/cuddle/instruction/Application.java
+++ src/main/java/ai/cuddle/instruction/Application.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
  * Created by suman.das on 1/5/17.
  */
 @SpringBootApplication
-@ComponentScan({"com.cuddle.instruction"})
+@ComponentScan({"ai.cuddle.instruction.*"})
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class);
--- src/main/java/ai/cuddle/instruction/repository/InstructionRepository.java
+++ src/main/java/ai/cuddle/instruction/repository/InstructionRepository.java
@@ -1,10 +1,7 @@
 package ai.cuddle.instruction.repository;
 
-import ai.cuddle.instruction.entity.Instruction;
-import org.springframework.data.repository.CrudRepository;
-
 /**
  * Created by suman.das on 1/6/17.
  */
-public interface InstructionRepository extends CrudRepository<Instruction,Long>{
+public interface InstructionRepository {
 }
