1de1c7248189d0c743b307367bfd88a1809b5820
sumand <suman.das@fractalanalytics.com>
2017-01-09 17:48:19 +0530

    Initial Commit

--- src/main/java/ai/cuddle/instruction/InstructionApplication.java
+++ src/main/java/ai/cuddle/instruction/InstructionApplication.java
@@ -3,7 +3,6 @@ package ai.cuddle.instruction;
 import ai.cuddle.instruction.config.RibbonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -12,7 +11,6 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @ComponentScan({"ai.cuddle.instruction.*"})
-@EnableDiscoveryClient
 @RibbonClient(name = "client", configuration = RibbonConfig.class)
 public class InstructionApplication {
     public static void main(String[] args){
