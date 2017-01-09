1353dbaa1d7138c64c3d63bb3fb5e1bb179b0ad1
sumand <suman.das@fractalanalytics.com>
2017-01-09 11:27:42 +0530

    Initial Commit

---
+++ src/test/java/ai/cuddle/instruction/test/InstructionTest.java
@@ -0,0 +1,16 @@
+package ai.cuddle.instruction.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by suman.das on 1/6/17.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class InstructionTest {
+
+
+
+}
