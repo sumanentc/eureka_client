43ef628706751d54bcd4fa0af768c37982a86ecc
Unni Krishnan <unni.krishnan@cuddle.ai>
2017-01-09 18:29:08 +0530

    master:log files ignore

--- src/main/java/ai/cuddle/instruction/InstructionApplication.java
+++ src/main/java/ai/cuddle/instruction/InstructionApplication.java
@@ -3,6 +3,7 @@ package ai.cuddle.instruction;
 import ai.cuddle.instruction.config.RibbonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan({"ai.cuddle.instruction.*"})
 @RibbonClient(name = "client", configuration = RibbonConfig.class)
+@EnableDiscoveryClient
 public class InstructionApplication {
     public static void main(String[] args){
         SpringApplication.run(InstructionApplication.class);
